f197ed48b71625f2027ef8c7406c4e738bd55c97
Namth0 <86194103+Namth0@users.noreply.github.com>
2024-02-05 19:21:28 +0100

    ajout de la fonction color : qui attend un url en param et qui retourne une (char) couleur  (penser à simplifier le code )

--- src/PremierJalon.py
+++ src/PremierJalon.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 url = {
     "https://ultimate-mushroom.com/poisonous/103-abortiporus-biennis.html",
     "https://ultimate-mushroom.com/edible/1010-agaricus-albolutescens.html",
-    "https://ultimate-mushroom.com/inedible/452-byssonectria-terrestris.html"
+    "https://ultimate-mushroom.com/inedible/452-byssonectria-terrestris.html",
+    "https://ultimate-mushroom.com/poisonous/1010-agaricus-albolutescens.html"
 }
 
 def comestible(url):
@@ -26,7 +27,28 @@ def comestible(url):
     except Exception as e:
         print(f"Error processing URL: {e}")
         return ""
+    
+def color(url):
+    try:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        div_content = soup.find('div', class_='mprofile')
+        if div_content:
+            p_tag = div_content.find('p')
+            if p_tag:
+                a_tag = p_tag.find('a')
+                if a_tag and a_tag.find_next_sibling('a'):
+                    color = a_tag.text + "-" + a_tag.find_next_sibling('a').text
+                else:
+                    color = a_tag.text
+        return color
+    except Exception as e:
+        print(f"Error processing URL: {e}")
+        return ""
 
 
 for u in url:
     print(f"{u} -> {comestible(u)}")
+
+for i in url:
+    print(f"{i} -> {color(i)}")
